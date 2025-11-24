c1270393ef214b04519d69c29dfde8f5e7c4c8c0
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-24 11:16:04 +0100

    build(backend): initial state of the Backend
    Created the structure and the initial server
    
    BREAKING CHANGE: Initial working version

---
+++ README.md
@@ -0,0 +1,3 @@
+## Home Pulse Server
+
+this is going to be a nice structured server for our app
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import router from './router';
+
+export const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const port = 3000;
+
+async function startServer() {
+  try {
+
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}.🏃`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+  }
+}
+
+startServer();
