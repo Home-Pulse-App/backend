3ca1789e4020ca1d22e7c02dbff5b97b2048a3b2
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-24 13:15:28 +0100

    feat(backend): add jwt
    added the routes and return the token with a mock user
    
    BREAKING CHANGE: JWT auth

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import 'dotenv/config';
+import { login } from './controller/auth.controller';
+import { auth } from './middleware/auth.middleware';
 
 const router = Router();
 
@@ -8,7 +10,8 @@ router.get('/', (req, res)=>{res.status(200).json('It is alive! 🧟')});
 
 // Users
 //TODO add CRUD to users
-
+router.post('/login',login);
+router.get('/profile',auth)
 
 // Homes
 //TODO add CRUD to Homes
