a8bb1a76e7a191d37b36fda98fb5240068d1c550
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-25 11:41:55 +0100

    feat(mqtt utils): add mqtt connection
    Added the mqtt connection qith the config and the future controller

---
+++ src/utils/mqtt.ts
@@ -0,0 +1,26 @@
+import mqtt from 'mqtt';
+import mqttConfig  from '../config/mqtt.config';
+
+const client = mqtt.connect(mqttConfig.brokerUrl, mqttConfig.options);
+
+client.on('connect', () => {
+  console.log('✅ MQTT conectado a', mqttConfig.brokerUrl);
+  
+  // Suscripciones iniciales
+  client.subscribe('devices/+/status');
+  client.subscribe('comands/#');
+});
+
+client.on('error', (err) => {
+  console.error('❌ Error MQTT:', err);
+});
+
+client.on('offline', () => {
+  console.warn('MQTT desconectado');
+});
+
+client.on('reconnect', () => {
+  console.log('Reconectando MQTT...');
+});
+
+export default client;
