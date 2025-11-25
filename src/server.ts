f45c821a6fe5b10ba38a3707fa4e0a8ac8907379
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-25 09:58:04 +0100

    fix(routing): fix routing
    fixed the routing of the server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import router from './routes';
+import router from './routes/index';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger.config';
 import connectDB from './db';
