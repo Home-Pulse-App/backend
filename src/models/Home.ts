07aad3e0b2c689625ceb93253023f1013517ea19
Artak Navoyan <navoyan.ab@gmail.com>
2025-11-26 15:55:26 +0100

    feat: add endpoint to create a room in a home

--- src/models/Home.ts
+++ src/models/Home.ts
@@ -1,9 +1,9 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document, Types } from 'mongoose';
 
 export interface IHome extends Document {
   homeName: string;
-  userId: Schema.Types.ObjectId;
-  rooms: Schema.Types.ObjectId[];
+  userId: Types.ObjectId;
+  rooms: Types.ObjectId[];
 }
 
 const homeSchema = new Schema<IHome>(
